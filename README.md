de16ec6260ef69739625cd1275676d67f17a4322
Luka Ivanovic <ivanovic@tutanota.de>
2021-11-16 21:15:41 +0100

    Done for now..

--- README.md
+++ README.md
@@ -4,16 +4,17 @@ Simple hacky script that generates random noise images (or *snow* image).
 
 ## Usage
 
-Given that there are no requirments other than random and argparse (which are
+Given that there are no requirments other than `random` and `typing` (which are
 part of python's standard libs), there is no need to create/activate 
 virtualenv, you just need `python3` interpreter.
 
-python3 main.py -w(idth) 500 -h(eight) 500 -b(lock_size) 10 -o(utput) my_image
+`python3 main.py -w(idth) 500 -h(eight) 500 -b(lock_size) 10 -o(utput) my_image`
 
 defaults:
-    -w 100
-    -h 100
-    -b 10
+  - -w 100
+  - -h 100
+  - -b 1
+  - -o out.ppm
 
 Generated image uses `ppm` format for the sake of simplicity, even tho there are
 a plethora of other simple and fast libs for manipulating images. Because the 
@@ -27,3 +28,4 @@ Todo:
   at the end)
   - Add option to specify color mode of the resulting image (currently it 
   supports only grayscale like *true* snow)
+  - Optimize generating algorithm (remove damn loops..)
--- main.py
+++ main.py
@@ -1,51 +1,78 @@
 from random import seed, randint
+import sys
+from typing import List, Tuple
 
-seed(420)
+def parse_args (args: List[str]) -> Tuple[int, int, int, str]:
+    width = 100
+    height = 100
+    block_size = 1
+    outfile = 'out.ppm'
+    args = args[1:]
+    if len(args)%2!=0 :
+        print("Proper flags format is not registered. Read README.md .")
+        exit(1)
+    else:
+        index=0
+        while index < len(args):
+            value = args[index]
+            if (value=='-w' or value=='-width'):
+                if (args[index+1].isdecimal()):
+                    width = int(args[index+1])
+                else:
+                    print("You need to provide width with -w")
+                    exit(1)
+                index+=2
+            elif (value=='-h' or value=='-height'):
+                if (args[index+1].isdecimal()):
+                    height = int(args[index+1])
+                else:
+                    print("You need to provide height with -h")
+                    exit(1)
+                index+=2
+            elif (value=='-b' or value=='-block_size'):
+                if (args[index+1].isdecimal()):
+                    block_size = int(args[index+1])
+                    if (width%block_size!=0 or height%block_size!=0):
+                        print("Width and height should be divisible by block size")
+                        exit(1) 
+                else:
+                    print("You need to provide block size with -b")
+                    exit(1)
+                index+=2
+            elif (value=='-o' or value=='-output'):
+                if (args[index+1].isalpha()):
+                    outfile = args[index+1]+'.ppm'
+                else:
+                    print("You need to provide filename with -o")
+                    exit(1)
+                index+=2
+            else:
+                print("Proper flags format is not registered. Read README.md .")
+                exit(1)
+    return (width,height,block_size,outfile)
 
+def generate_image (width: int, height: int, block_size: int) -> List[List[int]] :
+    image = [[(x,y) for y in range(width)] for x in range(height)]
+    # super unoptimized way of doing this :((
+    for i in range(height//block_size):
+        for j in range(width//block_size):  
+            color = str(randint(0,255))
+            for row in image[i*block_size:i*block_size+block_size]:
+                for el in row[j*block_size:j*block_size+block_size]:
+                    image[el[0]][el[1]]=color
+    return image
 
-def echo (*some):
-    for s in some:
-        print(s,end="")
+def save_image (image: List[List[int]], outfile: str):
+    with open(outfile,"wt") as f:
+        f.write("P2 "+str(len(image[0]))+" "+str(len(image))+" 255\n")
+        for row in image:
+            for el in row:
+                f.write(str(el)+" ")
 
-def ppp (some):
-    for s in some:
-        print(s,end="\n")
-        
+def main():
+    width, height, block_size, outfile = parse_args(sys.argv)
+    image = generate_image(width, height, block_size)
+    save_image(image, outfile)
 
-f = open("thump.ppm","wt")
-
-width = 500
-height = 500
-
-
-image = [[(x,y) for y in range(width)] for x in range(height)]
-# ppp(image)
-
-f.write("P2 "+str(width)+" "+str(height)+" 255\n")
-
-# for i in range(0,width*height,10):
-#     color=str(randint(0,255))
-#     for j in range(10):
-#         image[i+j]=color
-#         image[i+width+j]=color
-#     # f.write(str(randint(0,255))+"\n")
-
-block_size=10
-
-for i in range(height//block_size):
-    for j in range(width//block_size):  
-        color = str(randint(0,255))
-        for row in image[i*block_size:i*block_size+block_size]:
-            for el in row[j*block_size:j*block_size+block_size]:
-                image[el[0]][el[1]]=color
-
-for row in image:
-    for el in row:
-        f.write(str(el)+" ")
-
-
-
-
-# ppp(image)
-
-f.close()
+if __name__ == "__main__":
+    main()
